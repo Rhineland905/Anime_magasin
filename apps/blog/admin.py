0381b27fa04333218a92b61dda0f5951b77b8e88
Rhineland905 <104513373+Rhineland905@users.noreply.github.com>
2022-11-22 19:12:38 +0200

    blok_part_2

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from apps.blog.models import BlogCategory
-from apps.blog.models import Article , Teg
+from apps.blog.models import Article, Teg
 
 admin.site.register(BlogCategory)
 admin.site.register(Article)
