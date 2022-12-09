a8078e0c80f5a35bc4184d80f8404654bba41aec
Rhineland905 <104513373+Rhineland905@users.noreply.github.com>
2022-12-09 18:10:19 +0200

    static_and_bootsrap

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', blog_category_list, name="blog_category_list"),
     path("<int:category_id>/", article_list, name="blog_article_list"),
     path("<int:category_id>/<int:article_id>/", article_viwe, name="blog_article_viwe"),
-    path("<str:teg_id>", teg_shear, name="teg_view")
+    path("tag/<str:teg_id>", teg_shear, name="teg_view")
 ]
