c1d7745017f319b90406b5b065ccc4e20ca94d20
Rhineland905 <104513373+Rhineland905@users.noreply.github.com>
2022-12-16 18:18:08 +0200

    blog_images

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from config.views import index
+from django.conf.urls.static import static
+from config import settings
 
 urlpatterns = [
     path('',index,name='index'),
     path('admin/', admin.site.urls),
     path('blog/',include('apps.blog.urls'))
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
