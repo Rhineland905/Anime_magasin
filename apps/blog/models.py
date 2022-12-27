b86f8e7c60d6eb97a3cf1979de80bb8a64f9af7d
Rhineland905 <104513373+Rhineland905@users.noreply.github.com>
2022-12-27 16:45:33 +0200

    updates_avatar

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -52,7 +52,7 @@ class Article(models.Model):
     category = models.ForeignKey(to=BlogCategory, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(verbose_name='Заголовок', max_length=255)
     text_preview = models.TextField(verbose_name='Текст-превью', null=True, blank=True)
-    user = models.ForeignKey(to=User, verbose_name='Пользователь', on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(to=User, verbose_name='Пользователь', on_delete=models.SET_NULL, null=True,blank=True)
     text = models.TextField(verbose_name='Текст')
     publish_date = models.DateTimeField(verbose_name='Дата публикации')
     tags = models.ManyToManyField(to=Tag, verbose_name='Теги', blank=True)
