3985ff79dbe3a7eada773fbafd4f63ae872e069d
Rhineland905 <104513373+Rhineland905@users.noreply.github.com>
2023-01-05 10:21:37 +0200

    product_views

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.catalog.views import CategoryIndexView
+from apps.catalog.views import CategoryIndexView, ProductsByCategoryView, ProductDetailView
 
 urlpatterns = [
-    path('',CategoryIndexView.as_view(),name='catalog'),
-    path('<str:slug>/',CategoryIndexView.as_view(),name='categories'),
+    path('', CategoryIndexView.as_view(), name='catalog'),
+    path('<str:slug>/', ProductsByCategoryView.as_view(), name='categories'),
+    path('product/<str:slug>/', ProductDetailView.as_view(), name='product')
 ]
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,8 +1,28 @@
 from django.views import generic
-from apps.catalog.models import Category
+from apps.catalog.models import Category, Product
+
 
 class CategoryIndexView(generic.ListView):
     model = Category
     template_name = 'catalog/index.html'
     queryset = Category.objects.filter(parent=None)
 
+class ProductsByCategoryView(generic.ListView):
+    template_name = 'catalog/category.html'
+    category = None
+    categories = Category.objects.all()
+
+    def get_queryset(self):
+        self.category = Category.objects.get(slug=self.kwargs['slug'])
+        queryset = Product.objects.filter(categories=self.category)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        context['categories'] = self.categories
+        return context
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
