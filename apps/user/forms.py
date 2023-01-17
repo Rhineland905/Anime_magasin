ebee51f831989bbccefd02969088f7d0b67aa3d4
Rhineland905 <104513373+Rhineland905@users.noreply.github.com>
2023-01-17 18:07:29 +0200

    Registretion

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -1,5 +1,27 @@
 from django import forms
 
+from apps.user.models import User
+
+
 class LoginForm(forms.Form):
-    username = forms.CharField(required=True,max_length=64)
-    pasword = forms.CharField(required=True,max_length=32, widget=forms.PasswordInput)
+    username = forms.CharField(required=True, max_length=255)
+    password = forms.CharField(required=True, max_length=64, widget=forms.PasswordInput)
+
+class RegisterForm(forms.ModelForm):
+    password_confirm = forms.CharField(required=True, max_length=64, widget=forms.PasswordInput)
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        check = User.objects.filter(email=email)
+        if check:
+            raise forms.ValidationError('Пользователь с таким E-mail уже сушествует')
+        return email
+    def clean_password_confirm(self):
+        password = self.cleaned_data['password']
+        password_confirm = self.cleaned_data['password_confirm']
+        if password_confirm == password:
+            return password_confirm
+        else:
+            raise forms.ValidationError('Пароли не совпадают ')
+    class Meta:
+        model = User
+        fields = ['username','image','first_name','last_name','email','phone','password','password_confirm']
