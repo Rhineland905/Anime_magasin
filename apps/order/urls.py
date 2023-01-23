298e8e0f8aeab2d8d3bbeda2c484c65c4789d786
Rhineland905 <104513373+Rhineland905@users.noreply.github.com>
2023-01-23 12:11:09 +0200

    cart_html

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.order.views import add_to_cart
+from apps.order.views import add_to_cart, cart_button
 
 urlpatterns = [
     path('add-to-cart/', add_to_cart, name='add_to_cart'),
+    path('cart-button/', cart_button, name='cart_button'),
 ]
