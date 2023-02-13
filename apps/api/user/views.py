d3a4ee8b0b1563d4c13f14afba943f72bd88e469
Rhineland905 <104513373+Rhineland905@users.noreply.github.com>
2023-02-13 09:05:15 +0200

    drf_part_1

--- apps/api/user/views.py
+++ apps/api/user/views.py
@@ -1,25 +1,24 @@
-from rest_framework import generics,status
-from apps.api.user.serializers import LofinSerializer,TokenSerializer
+from rest_framework import generics, status
+from apps.api.user.serializers import LoginSerializer, TokenSerializer
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 
 
-
 class LoginView(generics.GenericAPIView):
-    serializer_class = LofinSerializer
+    serializer_class = LoginSerializer
 
-    def post(self,request):
+    def post(self, request):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_excepton=True)
+        serializer.is_valid(raise_exception=True)
         user = authenticate(**serializer.data)
         if user:
             token = Token.objects.filter(user=user).first()
             if not token:
-                token.objects.create(user=user)
-            token_serializer = TokenSerializer(data={'token':token.key})
+                token = Token.objects.create(user=user)
+            token_serializer = TokenSerializer(data={'token': token.key})
             token_serializer.is_valid()
-            return Response(data=token_serializer.data,status=status.HTTP_200_OK)
+            return Response(data=token_serializer.data, status=status.HTTP_200_OK)
         else:
-            raise AuthenticationFailed('Не правильный логин или пароль')
+            raise AuthenticationFailed('Неправильные логин или пароль')
