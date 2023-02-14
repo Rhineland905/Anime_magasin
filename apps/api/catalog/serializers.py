a43ff2c6c6af716acdbd7e90ee177c40ed70c1a7
Rhineland905 <104513373+Rhineland905@users.noreply.github.com>
2023-02-14 17:06:10 +0200

    drf_homework

--- apps/api/catalog/serializers.py
+++ apps/api/catalog/serializers.py
@@ -13,7 +13,6 @@ class CategorySerializer(serializers.ModelSerializer):
             'slug',
             'description',
             'parent',
-            'img',
             'meta_title',
             'meta_description',
             'meta_keywords',
